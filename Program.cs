7d3f4e38cc5142dd2afa254ae0bd947a03007995
Demis Bellot <demis.bellot@gmail.com>
2022-03-19 16:31:13 +0800

    Add OSS License Key

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using NorthwindAuto;
 using ServiceStack;
 
+Licensing.RegisterLicense("OSS BSD-3-Clause 2022 https://github.com/NetCoreApps/NorthwindAuto ckEe9QZMp81KLdfriyot8eJX05KVNXOB+7xWoOCTIxOSLNCO0JkC6mTtx3kwEI1erMFOkNCPORt2RqvObrrni18wt6PYgua1lNatq5LuNErrdQKKb6b7MbObmJA118VLMQCkPNplBYwl0TeWex6wfVhUWtxgzLbEp+5NCjWTIio=");
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
